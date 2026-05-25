806c8a70d9119907d9c821b7e462829566d2509c
Kit Rady <kitrady@gmail.com>
2026-05-25 14:12:31 -0500

    adding template files

---
+++ website/scripts.js
@@ -0,0 +1,16 @@
+async function loadData() {
+    const response = await fetch("/data.json");
+    const data = await response.json();
+
+    // TODO change this to match actual website
+    document.getElementById("restaurant-name").textContent =
+        data.restaurant;
+
+    document.getElementById("wait-time").textContent =
+        data.waitMinutes;
+
+    document.getElementById("queue-count").textContent =
+        data.ordersInQueue;
+}
+
+loadData();
